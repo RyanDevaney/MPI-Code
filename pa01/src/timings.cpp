340e0f860e9ed88d9780bc805b93b6a35f135588
RyanDevaney <rdevaney@nevada.unr.edu>
2017-02-27 17:47:48 -0800

    Update timings.cpp

--- pa01/src/timings.cpp
+++ pa01/src/timings.cpp
@@ -12,7 +12,6 @@ int main(int argc, char *argv[])
     double time;
     double firstTime,secondTime;
     MPI_Status status;
-    MPI_Request request;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
